5443b83d335636e02360b8bd099568903f928d21
Tyuiu.RubankoGV@gmail.com <Tyuiu.RubankoGV@gmail.com>
2024-12-27 02:32:26 +0300

    дши  и

---
+++ Tyuiu.RubankoGV.Sprint7.V8.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+
+using System;
+using Tyuiu.RubankoGV.Sprint7.V8.Lib;
+using System.IO;
+using NUnit.Framework;
+
+namespace Tyuiu.RubankoGV.Sprint7.V8.Test;
+
+public class DataServiceTest
+{
+    public void FileLoadedTest()
+    {
+        var ds = new DataService();
+        string path = @"C:\DataSprint7\AutoTest.csv";
+        List<string[]> wait = new List<string[]>();
+        wait.Add(new string[] { "98", "А344КО", "Porshe", "Отличное", "В гараже", "70", "40", "60" });
+        wait.Add(new string[] { "54", "45ЕНГ8", "КАМАЗ", "Требуется ТО", "Краснодар", "40", "200", "300" });
+        CollectionAssert.AreEqual(wait, ds.LoadDataFromFile(path));
+    }
+}
+
+
